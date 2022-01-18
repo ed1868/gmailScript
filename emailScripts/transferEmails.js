622189ee192e9e4d9c25fd837d4a6d5288aa82bf
ed1868 <ruizeduardo21@gmail.com>
2022-01-18 15:46:17 -0500

    starting transfer script

---
+++ emailScripts/transferEmails.js
@@ -0,0 +1,14 @@
+function transferEmails() {
+    const threads = GmailApp.getInboxThreads();
+    const myGmailLabels = GmailApp.getUserLabels();
+  
+    Logger.log(myGmailLabels);
+    
+    // LABEL I WANT TO TRANSFER ALL EMAILS TO
+    const oxLabel = "OX AC";
+    
+  
+    
+  }
+  
+  

b240edf082078d3c3919861e3b0667eedc2a9fac
ed1868 <ruizeduardo21@gmail.com>
2022-01-18 16:49:27 -0500

    done script

--- emailScripts/transferEmails.js
+++ emailScripts/transferEmails.js
@@ -7,6 +7,21 @@ function transferEmails() {
     // LABEL I WANT TO TRANSFER ALL EMAILS TO
     const oxLabel = "OX AC";
 
+
+    const testLabel = "poopybutthole";
+
+    const labelsHolderArr = [];
+
+
+    myGmailLabels.map(label => {
+        Logger.log(label.getName());
+        labelsHolderArr.push(label.getName())
+    })
+
+
+    let tempLabel;
+
+
     //CREATES NEW LABEL IF YOU DONT HAVE IT ALREADY 
     //LOGS IF YOU ALREADY HAVE THE NEW LABEL 
     if (labelsHolderArr.includes(oxLabel)) {
@@ -16,6 +31,38 @@ function transferEmails() {
         Logger.log('Label is being Created');
         tempLabel = GmailApp.createLabel(oxLabel);
     }
+
+
+    threads.map(message => {
+        //EMAIL TARGET
+        let target = "ox@overseasinternational.com";
+        let targetTwo = "ox@overseasnetwork.com";
+        let targetArray = ["Overseas Xpress <ox@overseasnetwork.com>", "ox@overseasnetwork.com", "ox@overseasinternational.com",]
+        //GET MESSAGE SENDERS 
+        var messageSenders = message.getMessages();
+        // Logger.log('MESSAGE SENDERS:');
+        // Logger.log(messageSenders);
+        if (messageSenders) {
+            //LOOP ALL MESSAGES
+            messageSenders.map(email => {
+                //GET THE SENDERS ADDRESS
+                let emailSender = email.getFrom();
+                Logger.log(emailSender);
+
+                //GET THE EMAILS CC ADDRESSES
+                let emailCc = email.getCc().split(',');
+
+                Logger.log(typeof (emailCc));
+                Logger.log(Object.values(emailCc));
+
+                //CHECK IF EMAIL ADDRESS IS OX@OVERSEASINTERNATIONAL.COM OR ox@overseasnetwork.com
+                if (emailSender == target || emailSender == targetTwo) {
+                    message.addLabel(tempLabel);
+                    message.moveToArchive();
+                }
+            })
+        }
+    })
 
 }
 

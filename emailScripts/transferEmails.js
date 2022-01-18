4d8febc63cd2fe9105ab17048c8552be214ffa67
ed1868 <ruizeduardo21@gmail.com>
2022-01-18 16:00:59 -0500

    label creater

--- emailScripts/transferEmails.js
+++ emailScripts/transferEmails.js
@@ -1,14 +1,21 @@
 function transferEmails() {
     const threads = GmailApp.getInboxThreads();
     const myGmailLabels = GmailApp.getUserLabels();
-  
+
     Logger.log(myGmailLabels);
-    
+
     // LABEL I WANT TO TRANSFER ALL EMAILS TO
     const oxLabel = "OX AC";
-    
-  
-    
-  }
-  
-  
+
+    //CREATES NEW LABEL IF YOU DONT HAVE IT ALREADY 
+    //LOGS IF YOU ALREADY HAVE THE NEW LABEL 
+    if (labelsHolderArr.includes(oxLabel)) {
+        Logger.log('Label is already created');
+        tempLabel = GmailApp.getUserLabelByName(oxLabel);
+    } else {
+        Logger.log('Label is being Created');
+        tempLabel = GmailApp.createLabel(oxLabel);
+    }
+
+}
+
